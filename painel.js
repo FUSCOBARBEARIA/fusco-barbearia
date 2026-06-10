452bc578dc75d752023ea079e88e60cf87de7311
FUSCOBARBEARIA <fuscobarbearia@gmail.com>
2026-06-09 21:46:19 -0300

    Update painel.js

--- painel.js
+++ painel.js
@@ -1,25 +1,31 @@
-async function carregarAgendamentos(){
+import {
+db,
+collection,
+getDocs
+} from "./firebase.js";
 
-const snapshot =
-await getDocs(
-collection(db, "agendamentos")
+const tabela = document.getElementById("listaAgendamentos");
+
+async function carregarAgendamentos() {
+
+```
+const snapshot = await getDocs(
+    collection(db, "agendamentos")
 );
 
 const agendamentos = [];
 
 snapshot.forEach((doc) => {
-agendamentos.push(doc.data());
+    agendamentos.push(doc.data());
 });
 
 agendamentos.sort((a, b) => {
 
-```
-if(a.data === b.data){
-    return a.hora.localeCompare(b.hora);
-}
+    if (a.data === b.data) {
+        return a.hora.localeCompare(b.hora);
+    }
 
-return a.data.localeCompare(b.data);
-```
+    return a.data.localeCompare(b.data);
 
 });
 
@@ -27,13 +33,11 @@ const agrupados = {};
 
 agendamentos.forEach((item) => {
 
-```
-if(!agrupados[item.data]){
-    agrupados[item.data] = [];
-}
+    if (!agrupados[item.data]) {
+        agrupados[item.data] = [];
+    }
 
-agrupados[item.data].push(item);
-```
+    agrupados[item.data].push(item);
 
 });
 
@@ -41,30 +45,29 @@ tabela.innerHTML = "";
 
 Object.keys(agrupados).forEach((data) => {
 
-```
-tabela.innerHTML += `
-<tr>
-    <th colspan="5">
-        📅 ${data}
-    </th>
-</tr>
-`;
-
-agrupados[data].forEach((agendamento) => {
-
     tabela.innerHTML += `
     <tr>
-        <td>${agendamento.hora}</td>
-        <td>${agendamento.nome}</td>
-        <td>${agendamento.telefone}</td>
-        <td>${agendamento.servico || "-"}</td>
-        <td>💈</td>
+        <th colspan="5">📅 ${data}</th>
     </tr>
     `;
 
+    agrupados[data].forEach((agendamento) => {
+
+        tabela.innerHTML += `
+        <tr>
+            <td>${agendamento.hora}</td>
+            <td>${agendamento.nome}</td>
+            <td>${agendamento.telefone}</td>
+            <td>${agendamento.servico || "-"}</td>
+            <td>💈</td>
+        </tr>
+        `;
+
+    });
+
 });
 ```
 
-});
-
 }
+
+carregarAgendamentos();
